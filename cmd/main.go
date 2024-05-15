45c66578318f7dc82acea514bfe62f5dac9f4ab4
Elena Khramkova <khramkovase@gmail.com>
2024-05-15 17:45:48 +0300

    add handlers tests

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"lamoda/internal/logger"
 	"lamoda/internal/server"
 	"lamoda/internal/service"
 	"lamoda/internal/storage"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 )
 
 // @title			API для управления товарами на складах
@@ -25,7 +28,7 @@ func main() {
 	godotenv.Load()
 	log := logger.InitLogger(os.Getenv("LOG_LEVEL"))
 
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
 	conn, err := pgx.Connect(ctx, os.Getenv("DSN_STRING"))
 	if err != nil {
 		log.Fatal(err.Error())
@@ -47,9 +50,38 @@ func main() {
 		Addr:    os.Getenv("RUN_ADDR"),
 		Handler: router,
 	}
-	fmt.Println(os.Getenv("RUN_ADDR"))
+	var exit chan bool
+	go gracefulShutdown(ctx, log, srv, conn, exit)
+
 	log.Info("Server started")
 	if err := srv.ListenAndServe(); err != nil {
-		log.Error(err.Error())
+		log.Fatal(err.Error())
 	}
+
+	cancel()
+	<-exit
+}
+
+func gracefulShutdown(ctx context.Context, log *logrus.Logger, server *http.Server, conn *pgx.Conn, exit chan bool) {
+	// SIGTERM for docker container default signal
+	signalCtx, cancel := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	// wait for parent or signal context to cancel
+	<-signalCtx.Done()
+	log.Info("shutting down http server...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	log.Info("closing pgx connection...")
+	conn.Close(shutdownCtx)
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Error(
+			"error shutting down http server",
+			"error", err,
+		)
+	}
+	exit <- true
 }
